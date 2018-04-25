4818ed958d53a8dc7812f4fadb8c6a260ddf749b
coffeeordeath <adamfisher9@gmail.com>
2018-04-24 22:47:07 -0700

    CIS_206_Bug_Project

---
+++ src/Bug.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+class Bug
+{
+    private int currentX;
+    private int currentY;
+    private String bugDirection;
+    
+    public Bug(int initialX, int initialY){
+        currentX = initialX;
+        currentY = initialY;
+        bugDirection = "to the right";
+    }
+    
+    public void bugMenu(){
+        
+        System.out.println("Hello! You are now in control of a bug.");
+        System.out.println("He is facing " + bugDirection + " and is at position (" + currentX + "," + currentY + ")");
+        System.out.println("What would you like it to do?");
+        System.out.println("It can do the following things: ");
+        System.out.println("Move forward, turn to the right, or turn to the left");
+        System.out.println("");
+        bugChoices();
+    }
+    
+    public void bugChoices(){
+        System.out.println("Make your choice now (type in 'right' to go right, 'left' to go left' or 'move' to move forward or type 'quit' to quit) : ");
+        String choice = in.nextLine();
+        bugSwitch(choice);
+        bugChoices();
+    }
+    
+    public void bugSwitch(String c){
+        
+        switch(c){
+            case "right":
+                bugTurnRight();
+                break;
+            case "left":
+                bugTurnLeft();
+                break;
+            case "move":
+                bugMove();
+                break;
+            case "quit":
+                System.exit(0);
+        }
+    }
+    
+    public void bugTurnRight(){
+        if(bugDirection.equals("Upwards")){
+            bugDirection = "to the right";
+        }
+        else if(bugDirection.equals("to the right")){
+            bugDirection = "Downwards";
+        }
+        else if(bugDirection.equals("Downwards")){
+            bugDirection = "to the left";
+        }
+        else if(bugDirection.equals("to the left")){
+            bugDirection = "Upwards";
+        }
+        System.out.println("Adam Ant is now facing " + bugDirection);
+    }
+    
+    public void bugTurnLeft(){
+        if(bugDirection.equals("Upwards")){
+            bugDirection = "to the left";
+        }
+        else if(bugDirection.equals("to the left")){
+            bugDirection = "Downwards";
+        }
+        else if(bugDirection.equals("Downwards")){
+            bugDirection = "to the right";
+        }
+        else if(bugDirection.equals("to the right")){
+            bugDirection = "Upwards";
+        }
+        System.out.println("Adam Ant is now facing " + bugDirection);
+    }
+    
+    public void bugMove(){
+        if(bugDirection.equals("Upwards")){
+            currentY = currentY + 1;
+        }
+        else if(bugDirection.equals("to the right")){
+            currentX = currentX + 1;
+        }
+        else if(bugDirection.equals("to the left")){
+            currentX = currentX - 1;
+        }
+        else if(bugDirection.equals("Downwards")){
+            currentY = currentY - 1;
+        }
+        System.out.println("Adam Ant has moved to (" + currentX + "," + currentY +")");
+    }
+    
+    public int getX(){
+        return currentX;
+    }
+    
+    public int getY(){
+        return currentY;
+    }
+    
+    public String getDirection(){
+        return bugDirection;
+    }
+    
+    public Scanner in = new Scanner(System.in);
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+Filenames: Main.java and Bug.java
+ * Author: Adam Fisher
+ * Email: 0070342@student.vvc.edu
+ * Description: 
+ * Write a class Bug that models a bug moving along a horizontal line.
+ * The bug moves either to the right or to the left. Initially, the bug moves to
+ * the right, but it can turn to change its direction.In each move, its position
+ * changes by one unit in the current direction.
+ *******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+            Bug adamAnt = new Bug(0,0);
+	    adamAnt.bugMenu();
+	}
+}
